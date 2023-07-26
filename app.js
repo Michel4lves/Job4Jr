1d58ff040ee09ac70d2b4993cb17a3b4973a6966
Michel4lves <michelsantosa@gmail.com>
2023-07-26 18:23:11 -0300

    Add model and db

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const app     = express();
-const db      = require('./db/connection');
+const express       = require('express');
+const app           = express();
+const db            = require('./db/connection');
+const bodyParser    = require('body-parser');
 
-const PORT    = 3000;
+const PORT      = 3000;
 
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db
@@ -22,3 +26,7 @@ db
 app.get('/', function(req, res) {
     res.send('Its works!');
 });
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
+
