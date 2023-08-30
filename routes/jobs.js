305645c31440607b877c5ec1e1e69a71628c72b4
Michel4lves <michelsantosa@gmail.com>
2023-08-30 16:52:19 -0300

    add search

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,6 +2,9 @@ const express   = require('express'); // importando
 const router    = express.Router(); // inicializando objeto de rota do express
 const Job       = require('../models/Job'); // importando o modelo
 
+const sequelize = require('sequelize'); // importando o sequelize poara configurar a busca
+const Op        = sequelize.Op; // iniciando o pacote Op, do sequelize, para buscas complexas
+
 // test route
 // router.get('/test', (req, res) => { // pegando dados da rota 
 //     res.send('deu certo')
@@ -34,14 +37,34 @@ router.post('/add', (req, res) => { // "/add" => nome da rota ou a rota em si
 
 // My routes:
 router.get('/alljobs', function(req, res) {
-    Job.findAll({  // a partir do model Job
-        order: [
-            ['createdAt', 'DESC'] // usando o createdAt para ordenar de forma decrescente
-        ]
-    })
-    .then(jobs => {
-        res.render('alljobs', {jobs});
-    })
+
+    let search = req.query.job;
+    let query = '%' + search + '%'; // PH -/. PHP, word -> wordpress, press -> wordpress
+
+    if (!search) {
+        Job.findAll({  // a partir do model Job
+            order: [
+                ['createdAt', 'DESC'] // usando o createdAt para ordenar de forma decrescente
+            ]
+        })
+        .then(jobs => {
+            res.render('alljobs', {jobs});
+        })
+        .catch(err => console.log(err));
+    } else {
+        Job.findAll({  // a partir do model Job
+            where: {title: {[Op.like]: query}},
+            order: [
+                ['createdAt', 'DESC'] // usando o createdAt para ordenar de forma decrescente
+            ]
+        })
+        .then(jobs => {
+            res.render('alljobs', {jobs, search});
+        })
+        .catch(err => console.log(err));
+    }
+
+
 })
 
 
